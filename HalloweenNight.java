da459e42d9e256798a00f998b40dfaf7b4e1b70a
Mickey Paulus <136655973+BussySAKA1@users.noreply.github.com>
2023-10-24 20:53:16 -0400

    Update HalloweenNight.java

--- HalloweenNight.java
+++ HalloweenNight.java
@@ -1,12 +1,28 @@
+/**
+ * The HalloweenNight class represents the culmination of the Halloween festivities.
+ * @author mpaulus7
+ * @version 10.24
+ */
 public class HalloweenNight {
     private TrickOrTreater[] cryptKickerFive;
     private TrickOrTreater[] ghoulGang;
 
+    /**
+     * Constructs a HalloweenNight instance with two teams of trick-or-treaters.
+     *
+     * @param cryptKickerFive Array of trick-or-treaters in the cryptKickerFive team.
+     * @param ghoulGang Array of trick-or-treaters in the ghoulGang team.
+     */
     public HalloweenNight(TrickOrTreater[] cryptKickerFive, TrickOrTreater[] ghoulGang) {
         this.cryptKickerFive = cryptKickerFive;
         this.ghoulGang = ghoulGang;
     }
 
+    /**
+     * Provides a formatted String representation of the HalloweenNight.
+     *
+     * @return A formatted String showing the members of each team.
+     */
     @Override
     public String toString() {
         String returnString = "cryptKickerFive: ";
@@ -17,7 +33,7 @@ public class HalloweenNight {
                 returnString += cryptKickerFive[i];
             }
         }
-        returnString += " versus ";
+        returnString += " versus ghoulGang: ";
         for (int i = 0; i < ghoulGang.length; i++) {
             if (i != ghoulGang.length) {
                 returnString += ghoulGang[i] + ", ";
@@ -28,6 +44,9 @@ public class HalloweenNight {
         return returnString;
     }
 
+    /**
+     * Compares the trick-or-treaters in each team and determines which team is favored.
+     */
     public void compareTeams() {
         int ckfScore = 0;
         int ggScore = 0;
@@ -39,14 +58,19 @@ public class HalloweenNight {
             }
         }
         if (ckfScore > ggScore) {
-            System.out.println("cryptKickerFive is favored."); 
+            System.out.println("cryptKickerFive is favored.");
         } else if (ckfScore < ggScore) {
-            System.out.println("ghouldGand is favored.");
+            System.out.println("ghoulGang is favored.");
         } else {
             System.out.println("neither team is favored.");
         }
     }
 
+    /**
+     * Initiates a battle between the two teams with a specified winning threshold.
+     *
+     * @param winningThreshold The minimum candy count required for a team to win the battle.
+     */
     public void battle(int winningThreshold) {
         winningThreshold = (winningThreshold > 0) ? winningThreshold : 60;
 
@@ -71,12 +95,12 @@ public class HalloweenNight {
                     ((Ghost) ghoulGang[i]).rob(cryptKickerFive[i]);
                 }
             }
-        } while (cryptKickerCandy < winningThreshold || ghoulGangCandy < winningThreshold);
+        } while (cryptKickerCandy < winningThreshold && ghoulGangCandy < winningThreshold);
 
         if (cryptKickerCandy > ghoulGangCandy) {
-            System.out.println("cryptKickerFive won!"); 
+            System.out.println("cryptKickerFive won!");
         } else if (cryptKickerCandy < ghoulGangCandy) {
-            System.out.println("ghouldGand won!");
+            System.out.println("ghoulGang won!");
         } else {
             System.out.println("It is a tie!");
         }
