af557d225b66fcb22cf0cbbdf3c8a5570eb21293
Mickey Paulus <136655973+BussySAKA1@users.noreply.github.com>
2023-10-24 20:53:37 -0400

    Update Robbable.java

--- Robbable.java
+++ Robbable.java
@@ -1,3 +1,14 @@
+/**
+ * The Robbable interface represents an entity that can be robbed.
+ * @author mpaulus7
+ * @version 10.24
+ */
 public interface Robbable {
-    public int beRobbed();
+
+    /**
+     * Defines the action of being robbed.
+     *
+     * @return The number of items (e.g., candies) lost during the robbery.
+     */
+    int beRobbed();
 }
